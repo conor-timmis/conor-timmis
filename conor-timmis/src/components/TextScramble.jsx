ac9094e6a463a3bded069f213e604eae7b8f409a
Conor Timmis <55244147+conor-timmis@users.noreply.github.com>
2024-11-04 15:21:48 +0000

    Add TextScramble Component

---
+++ conor-timmis/src/components/TextScramble.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const TextScramble = () => {
+  const [text, setText] = useState('');
+  const finalText = 'Conor Timmis';
+  const chars = '!<>-_\\/[]{}—=+*^?#________';
+
+  useEffect(() => {
+    let iteration = 0;
+    const interval = setInterval(() => {
+      setText((prevText) =>
+        finalText
+          .split('')
+          .map((letter, index) => {
+            if (index < iteration) {
+              return finalText[index];
+            }
+            return chars[Math.floor(Math.random() * chars.length)];
+          })
+          .join('')
+      );
+
+      if (iteration >= finalText.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+
+    return () => clearInterval(interval);
+  }, [finalText]);
+
+  return (
+    <div className="text-2xl font-semibold tabular-nums tracking-tight text-red-500 text-center">
+      {text}
+    </div>
+  );
+};
+
+export default TextScramble;
